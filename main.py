0013033c66e90f15ad317fc596e3fa28b37cd31d
edgarslabans <edgars.labans@gmail.com>
2022-11-09 00:01:15 +0200

    Update 2022 - teh last one

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# tutorial of simple fishing bot for mrpg
+# simple script of auto filling dates of homeworking and
 
 import time
 import pyautogui as pyautogui
@@ -30,11 +30,30 @@ febT = [2,4,9,11,15,16,22,24,25,28]
 martW = [1,3,7,9,22,24,28,29,31]
 martT = [2,4,8,10,11,14,15,16,17,18,21,23,25,30]
 
+aprilW = [1,6,8,11,13,15,28]
+aprilT = [4,5,12,14,19,20,21,22,25,26,29]
 
-toWork = martT
-month = "03"
+meiW = [1,4,6,10,11,13]
+meiT = [3,5,9,12,16,17,18,19,20,26,27,30,31]
+
+juniW = [1,2,7,8,10,13,14,16,29]
+juniT = [3,9,15,17,20,21,27,28,30]
+
+juliW = [1,4,6,8,20,22,25,27,28]
+juliT = [5,7,11,12,13,14,15,18,19,21,26,29]
+
+
+toWork = juliW
+month = "07"
 year = "2022"
-typeInp = "home"  # work or home
+typeInp = "work"  # work or home
+
+
+int_coord_reis_button = [280,400]
+int_coord_date_field = [480, 170]
+int_coord_accept_button = [1680, 215]
+
+
 
 
 def checkIfDuplicates_1(listOfElems):
@@ -63,7 +82,7 @@ def singleClick():
 def checkColorAtCoord(x, y, col1):
     i = 1
     while i < 10:
-        print("waiting for " + str(col1) + " .. " + str(i) + " seconds")
+        print("waiting for rbg " + str(col1) + " at coord " + str(x) + "," + str(y) + " .. " + str(i) + " seconds")
         i += 1
         time.sleep(1)
         if getColor(x, y) == col1:
@@ -76,10 +95,10 @@ def checkColorAtCoord(x, y, col1):
 def FindCoordOfThuisdagKnop():
     i = 1
     while i < 100:
-        print("Looking for blue button at coordinate " + str(600 + i * 5))
+        print("Looking for Thuiswerkdag button at coordinate " + str(int_coord_reis_button[1]+100 + i * 5))
         i += 1
-        if getColor(490, 600 + i * 5) == 5:
-            return 600 + i * 5
+        if getColor(int_coord_reis_button[0], int_coord_reis_button[1]+100 + i * 5) == 5:
+            return int_coord_reis_button[1]+100 + i * 5
     print("Blue button not found - quiting ")
     quit()
 
@@ -90,7 +109,7 @@ def clickAndFill():
     print("Lets start ")
     checkIfDuplicates_1(toWork)
 
-    y_coord_knop = 420
+    y_coord_knop = int_coord_reis_button[1]
 
     if typeInp == "home":
         y_coord_knop = FindCoordOfThuisdagKnop()
@@ -100,14 +119,14 @@ def clickAndFill():
         # check of the background is white
         checkColorAtCoord(100, 200, 255)
         # check if the button is blue
-        checkColorAtCoord(490, y_coord_knop, 5)
+        checkColorAtCoord(int_coord_reis_button[0], y_coord_knop, 5)
 
         # reisdagen nieuw
-        pyautogui.moveTo(490, y_coord_knop)  # reisdagen 490, 420 // thuiswerkdagen 490 >600
+        pyautogui.moveTo(int_coord_reis_button[0], y_coord_knop)  # reisdagen 490, 420 // thuiswerkdagen 490 >600
         singleClick()
 
         # datum veld
-        pyautogui.moveTo(480, 340)
+        pyautogui.moveTo(int_coord_date_field[0], int_coord_date_field[1])
         singleClick()
         # check if the background is grey - window open
         checkColorAtCoord(100, 200, 183)
@@ -116,11 +135,11 @@ def clickAndFill():
         if len(day) == 1:
             day = str(0) + day
         pyautogui.write(day + "-" + month + "-" + year)
-        print("Filling", day + "-" + month + "-" + year)
+        print("Filling", day + "-" + month + "-" + year + " at coord: " + str(int_coord_date_field[0]) + "," + str(int_coord_date_field[1]))
         time.sleep(1)
 
         # accept datum
-        pyautogui.moveTo(1640, 430)     # HD1080 y= 390  HD1200 y = 430
+        pyautogui.moveTo(int_coord_accept_button[0], int_coord_accept_button[1])     # HD1080 y= 390  HD1200 y = 430
         singleClick()
         time.sleep(3)
 
